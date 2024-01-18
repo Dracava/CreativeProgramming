81df73b4e023b77f3201e08289fb5b75efd12288
Dracava <50298531+Dracava@users.noreply.github.com>
2024-01-18 14:18:07 +0100

    Add files via upload

---
+++ ico.js
@@ -0,0 +1,70 @@
+const radius = 450;
+var subdivisions = 1;//starting from 0
+
+//https://www.glprogramming.com/red/chapter02.html
+var triangles = [];
+function createIcosaedrum(){
+	var X = .525731112119133606*radius;
+	var Z = .850650808352039932*radius;
+	var vdata = [    
+		createVector(-X, 0.0, Z), 
+		createVector(X, 0.0, Z),
+		createVector(-X, 0.0, -Z),
+		createVector(X, 0.0, -Z),    
+		createVector(0.0, Z, X),
+		createVector(0.0, Z, -X),
+		createVector(0.0, -Z, X),
+		createVector(0.0, -Z, -X),    
+		createVector(Z, X, 0.0),
+		createVector(-Z, X, 0.0),
+		createVector(Z, -X, 0.0),
+		createVector(-Z, -X, 0.0) 
+	];
+	subdivide(vdata[0],		vdata[4],		vdata[1], 	subdivisions);
+	subdivide(vdata[0],		vdata[9],		vdata[4], 	subdivisions);
+	subdivide(vdata[9],		vdata[5],		vdata[4], 	subdivisions);
+	subdivide(vdata[4],		vdata[5],		vdata[8], 	subdivisions);
+	subdivide(vdata[4],		vdata[8],		vdata[1], 	subdivisions);
+	subdivide(vdata[8],		vdata[10],	vdata[1], 	subdivisions);
+	subdivide(vdata[8],		vdata[3],		vdata[10], 	subdivisions);
+	subdivide(vdata[5],		vdata[3],		vdata[8], 	subdivisions);
+	subdivide(vdata[5],		vdata[2],		vdata[3], 	subdivisions);
+	subdivide(vdata[2],		vdata[7],		vdata[3], 	subdivisions);
+	subdivide(vdata[7],		vdata[10],	vdata[3], 	subdivisions);
+	subdivide(vdata[7],		vdata[6],		vdata[10], 	subdivisions);
+	subdivide(vdata[7],		vdata[11],	vdata[6], 	subdivisions);
+	subdivide(vdata[11],	vdata[0],		vdata[6], 	subdivisions);
+	subdivide(vdata[0],		vdata[1],		vdata[6], 	subdivisions);
+	subdivide(vdata[6],		vdata[1],		vdata[10], 	subdivisions);
+	subdivide(vdata[9],		vdata[0],		vdata[11], 	subdivisions);
+	subdivide(vdata[9],		vdata[11],	vdata[2], 	subdivisions);
+	subdivide(vdata[9],		vdata[2],		vdata[5], 	subdivisions);
+	subdivide(vdata[7],		vdata[2],		vdata[11], 	subdivisions);	
+}
+
+function subdivide(v1, v2, v3, depth)
+{
+	if (depth == 0 || random()>0.5) {
+		triangles.push({v1:v1, v2:v2, v3:v3});
+		return;
+	}
+	var v12 = p5.Vector.add(v1, v2);
+	var v23 = p5.Vector.add(v2, v3);
+	var v31 = p5.Vector.add(v3, v1);
+	v12.normalize();
+	v23.normalize();
+	v31.normalize();
+	v12.mult(radius);
+	v23.mult(radius);
+	v31.mult(radius);
+	subdivide(v1, v12, v31, depth-1);
+	subdivide(v2, v23, v12, depth-1);
+	subdivide(v3, v31, v23, depth-1);
+	subdivide(v12, v23, v31, depth-1);
+}
+
+function keyPressed(){
+	triangles = [];
+	subdivisions = (subdivisions+1)%4;
+	createIcosaedrum();
+}
---
+++ mySketch.js
@@ -0,0 +1,111 @@
+/******************
+Code by Vamoss
+Original code link:
+https://www.openprocessing.org/sketch/672835
+
+Author links:
+http://vamoss.com.br
+http://twitter.com/vamoss
+http://github.com/vamoss
+******************/
+
+//Photography by Dazzle Jam
+//https://www.pexels.com/photo/grayscale-photo-of-topless-woman-1164674/
+
+var img;
+
+var canvasWebgl;
+
+const size = 600;//triangle size
+const duration = 3000;//loop duration
+
+easeInOut = t => t > 0.5 ? 4*pow((t-1),3)+1 : 4*pow(t,3);
+
+function preload() {
+	img = loadImage("pexels-dazzle-jam-1164674.png");
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	
+	canvasWebgl = createGraphics(img.width + 300, img.height, WEBGL);
+	canvasWebgl.stroke(255);
+	canvasWebgl.strokeWeight(3);
+	canvasWebgl.fill(0, 100);
+	createIcosaedrum();
+}
+
+function draw() {
+	background(230, 230, 125);
+	
+	//centralize
+	var zoom = min(windowWidth/img.width, windowHeight/img.height);
+	translate(width/2, height/2);
+	scale(zoom);
+	translate(-width/2, -height/2);
+	translate((width-img.width)/2, (height-img.height)/2);
+	
+	//loop
+	let loop = (millis()/duration)%1;
+	let progress = easeInOut(loop);
+	let dist = (1-progress) * size * 4 + size / 2;//progress * max distance + final size
+	let curSize = min(size, dist);
+	
+	noStroke();
+	fill(125, 255, 255);
+	push();
+		translate(img.width/2, img.height/2 - 100);
+		rotate(PI * progress / 3);
+
+		//center triangle
+		beginShape();
+		for(let a = 0; a < TWO_PI; a+=TWO_PI/3){
+			let x = sin(a) * curSize;
+			let y = cos(a) * curSize;
+			vertex(x, y);
+		}
+		endShape();
+
+		//lateral triangles
+		for(let i = 0; i < 3; i++){
+			let distX = sin(i/3 * TWO_PI + PI) * dist;
+			let distY = cos(i/3 * TWO_PI + PI) * dist;
+			beginShape();
+			for(let a = 0; a < TWO_PI; a+=TWO_PI/3){
+				let x = sin(a + PI) * curSize + distX;
+				let y = cos(a + PI) * curSize + distY;
+				vertex(x, y);
+			}
+			endShape();
+		}
+	pop();
+	
+	image(img, 0, 0);
+	
+	const face_dots = 30;
+	for(let i = 0; i < 7; i++){
+		fill(230, 230, 125+i*15);
+		for(let a = HALF_PI; a <= PI + HALF_PI; a += TWO_PI / face_dots){
+			let x = sin(a + PI + TWO_PI / face_dots * loop) * (60 + i * 20) + 330;
+			let y = cos(a + PI + TWO_PI / face_dots * loop) * (130 + i * 30) + 350;
+			var s = i+5;
+			if(a == HALF_PI) s *= loop;
+			if(a >= PI + HALF_PI) s *= 1-loop;
+			circle(x, y, s);
+		}
+	}
+	
+	
+	canvasWebgl.clear();
+	canvasWebgl.push();
+	canvasWebgl.rotateY(loop * TWO_PI);
+	canvasWebgl.beginShape(TRIANGLES);
+	for(var i = 0; i < triangles.length; i++) {
+		canvasWebgl.vertex(triangles[i].v1.x, triangles[i].v1.y, triangles[i].v1.z);
+		canvasWebgl.vertex(triangles[i].v2.x, triangles[i].v2.y, triangles[i].v2.z);
+		canvasWebgl.vertex(triangles[i].v3.x, triangles[i].v3.y, triangles[i].v3.z);
+	}
+	canvasWebgl.endShape();
+	canvasWebgl.pop();
+	image(canvasWebgl, -150, 0);
+}
